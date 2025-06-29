5a0cd9363ecc8a6e58892fce756b0cb190fd2a31
Olga1001 <zhuravel.olga95@gmail.com>
2025-06-29 23:13:29 +0300

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
 const appHeight = () => {
-  document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
+    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
 };
 appHeight();
