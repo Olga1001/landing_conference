2f5e39fd058daae85b32fe6612708f1dc0a02f76
Olga1001 <zhuravel.olga95@gmail.com>
2025-06-30 11:43:19 +0300

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,15 @@
+let prevWidth = window.innerWidth;
+
 const appHeight = () => {
   document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
 };
+
 appHeight();
-window.addEventListener('resize', appHeight);
+
+window.addEventListener('resize', () => {
+  const currentWidth = window.innerWidth;
+  if (currentWidth !== prevWidth) {
+    prevWidth = currentWidth;
+    appHeight();
+  }
+});
